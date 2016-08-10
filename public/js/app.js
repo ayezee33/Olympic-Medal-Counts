244d986717a1fb3fc2e178a2015b93824b072610
ayezee33 <ayezee33@gmail.com>
2016-08-10 12:02:55 -0700

    pointed resource files to correct location and have obj variables outputting to front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,30 @@
+//Make Foundation load
 $(document).foundation();
+//Ajax call
   $('#medalUpdate').on('click', function(e) {
     e.preventDefault();
     $.ajax({
       type: 'GET',
       url: '/api/v1/medals/united-states',
       success: function(data) {
-        console.log(data);
+        output(data);
       }
-    });
- });
+    }); //end ajax call
+    function output(data) {
+      var country = data.country_name
+      var gold    = data.gold_count
+      var silver  = data.silver_count
+      var bronze  = data.bronze_count
+      var place   = data.place
+      var total   = data.total_count
+
+    $('#output').html('<div class="callout large">' +
+      '<h3>' + country + '</h3>' +
+      '<p>Gold Count: ' + gold + '</p>' +
+      '<p>Silver Count: ' + silver + '</p>' +
+      '<p>Bronze Count: ' + bronze + '</p>' +
+      '<p>Place: ' + place + '</p>' +
+      '<p>Total Medals: ' + total + '</p>' +
+      '</div>');
+    }//end output function
+  });
